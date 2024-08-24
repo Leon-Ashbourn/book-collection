4417ecbeee888996ae33b022bae06156545f472f
Leon Ashbourne <mickeysane067@gmail.com>
2024-08-24 17:42:06 +0530

    fixed display of the books.added button to modify the reading status of the book.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # book-collection
+
+a simple library for books, where you can store and update your library daily. 
+you can check the book read status and update when completed with ease. that's pretty much how it works. 
+
+add book button would allow the user to enter the details of the book and can store the data, which will display on the page for your convinience. and can track your status of the book(though only whether you read or not). delete button would allow you to delete the book from the list and can't be re-tracked again. 
--- script.js
+++ script.js
@@ -1,43 +1,155 @@
 const inputSubmit = document.querySelector('#submit-input');
 const closeBtn = document.querySelector('#close-popup');
-
+let trackIndex = -1;
 const libraryBooks = [];
 
-function Book(author, title, pages, bookStatus) {
+function Book(author='-', title='-', pages='0', bookStatus='hold') {
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.bookStatus = bookStatus;
 }
 
+Book.prototype.index = 0;
 
 const html = document.querySelector('html');
 inputSubmit.addEventListener('click', (event)=>{
     event.preventDefault();
 
     const inputValues = document.querySelectorAll('form>div>input');
-    const status = document.querySelector(".status>input[type='radio']:checked")
+    const status = document.querySelector(".status>div> input[type='radio']:checked");
+
     let author = inputValues[0].value;
     let title = inputValues[1].value;
     let pages = inputValues[2].value;
-    let bookStatus = status.value;
-    console.log(bookStatus);
+    let bookStatus = "-";
+    if(!(status === null)){
+      bookStatus = status.value;
+    }
+
     const newBook = new Book(author, title, pages, bookStatus);
+
     addBooksToLibrary(newBook);
 
+    const eraser= function(){
+      for(let value of inputValues){
+        value.value = '';
+      }
+      inputValues[3].value = 'Confirm';
+      if(!(status === null)){
+        status.checked = false;
+      }
+    }
+
+    // formReset();
+
+    eraser();
+    displayBooks();
+    buttonColor();
+    hideElements();
 })
 
+// function formReset(){
+//   const resetBtn = document.createElement('input');
+//   resetBtn.type = 'reset';
+//   resetBtn.style = 'display: none';
+//   inputSubmit.parentNode.insertBefore(resetBtn, inputSubmit);
+//   const resetEvent = new MouseEvent('click', {
+//     view: windows,
+//     bubbles: true,
+//     cancelable: true,
+//   })
+// }
+
+function hideElements(){
+  const userContainer = document.querySelector('.user-input-container');
+  const formContainer = document.querySelector('.user-input');
+  userContainer.style.display ='none';
+  formContainer.style.display = 'none';
+}
+
 closeBtn.addEventListener('click',(e)=>{
-    console.log('here');
-    const userContainer = document.querySelector('.user-input-container');
-    const formContainer = document.querySelector('.user-input');
-    userContainer.style.display ='none';
-    formContainer.style.display = 'none';
+    hideElements();
+    buttonColor();
 });
 
-let trackIndex = 0;
+
 
 const addBooksToLibrary = function(newBook) {
-  libraryBooks[trackIndex] = newBook;
   trackIndex++;
+  libraryBooks[trackIndex] = newBook;  
+}
+
+const bookShelf = document.querySelector('.book-shelf');
+
+function displayBooks(){
+  bookShelf.textContent = '';
+  for( const property in libraryBooks){
+    const child = document.createElement('div');
+    
+    for(const [key,value] of Object.entries(libraryBooks[property])){
+      const subChild = document.createElement('p');
+      subChild.textContent = `${key}: ${value}`
+      child.appendChild(subChild);
+    }
+    const selfButton = document.createElement('button');
+    selfButton.setAttribute('data',`${property}`);
+    const readButton = document.createElement('button');
+    readButton.setAttribute('data',`${property}`);
+
+    selfButton.textContent = "Delete";
+    readButton.textContent = "Mark status";
+
+    child.appendChild(readButton);
+    child.appendChild(selfButton);
+    bookShelf.appendChild(child);
+  }
+}
+
+const addBookBtn = document.querySelector('.button> button');
+
+const buttonColor = function(){
+  const color = addBookBtn.id;
+  if(color === 'new-book-black'){
+    addBookBtn.id = "new-book-white"
+  }else {
+    addBookBtn.id = "new-book-black";
+  }
+}
+
+addBookBtn.addEventListener('click', ()=>{
+  buttonColor();
+  const userContainer = document.querySelector('.user-input-container');
+  const formContainer = document.querySelector('.user-input');
+  userContainer.style.display ='block';
+  formContainer.style.display = 'block';
+})
+
+const deleteBook = document.querySelector('.book-shelf');
+
+deleteBook.addEventListener('click', (event)=>{
+  const eventVal = event.target.getAttribute('data');
+  if(isNaN(Number(eventVal)) || eventVal === null){
+    return;
+  }else{
+    updateArray(Number(eventVal), event.target);
+  }
+})
+
+
+function updateArray(index, eventTarget){
+  if(eventTarget.textContent === 'Mark status'){
+    toggleReadStatus(index);
+  }else{
+    libraryBooks.splice(index,1);
+    displayBooks();
+  }
+}
+
+function toggleReadStatus(index){
+  libraryBooks[index].bookStatus === 'Completed' ? libraryBooks[index].bookStatus = 'Hold' : 
+                  libraryBooks[index].bookStatus = 'Completed';
+
+  displayBooks();
+
 }
